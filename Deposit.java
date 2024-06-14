91aa2d8e6f603b99c5e956923f182dd240eacda6
csjws <163653896+csjws@users.noreply.github.com>
2024-06-14 20:44:06 +0900

    Update Deposit.java

--- Deposit.java
+++ Deposit.java
@@ -1,17 +1,15 @@
-
-
 public class Deposit {
     private String depositID;
     private double amount;
 
     public Deposit(String accountNumber, double amount) {
-
+        this.amount = amount;
     }
 
     public void execute(Account account, double amount) {
-
-
+        account.deposit(amount);
     }
+
     public String getDepositID() {
         return depositID;
     }
