31754021a7ab72040a18390b18eae9450116c17c
csjws <163653896+csjws@users.noreply.github.com>
2024-06-14 20:44:25 +0900

    Update ExchangeRate.java

--- ExchangeRate.java
+++ ExchangeRate.java
@@ -1,7 +1,25 @@
-import java.util.*;
 public class ExchangeRate {
-    public ExchangeRate() {
+    private String currencyPair;
+    private double rate;
 
+    public ExchangeRate(String currencyPair, double rate) {
+        this.currencyPair = currencyPair;
+        this.rate = rate;
     }
 
+    public String getCurrencyPair() {
+        return currencyPair;
+    }
+
+    public void setCurrencyPair(String currencyPair) {
+        this.currencyPair = currencyPair;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
 }
