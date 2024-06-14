7c64e806741cb2eab616292e31e69b666efc8ef3
csjws <163653896+csjws@users.noreply.github.com>
2024-06-14 20:41:18 +0900

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,32 +1,43 @@
 import java.util.ArrayList;
 import java.util.List;
-
 public class Account {
     private String accountNumber;
     private double balance;
     private List<Object> transactionHistory;
 
     public Account(String accountNumber, double initialBalance) {
-
+        this.accountNumber = accountNumber;
+        this.balance = initialBalance;
+        this.transactionHistory = new ArrayList<>();
     }
 
     public void deposit(double amount) {
-
+        this.balance += amount;
+        transactionHistory.add(new Deposit(accountNumber, amount));
     }
 
     public void withdraw(double amount) {
-
+        if (amount <= this.balance) {
+            this.balance -= amount;
+            transactionHistory.add(new Withdraw(accountNumber, amount));
+        } else {
+            throw new IllegalArgumentException("잔액이 부족합니다.");
+        }
     }
 
     public void transfer(Account targetAccount, double amount) {
-
+        if (amount <= this.balance) {
+            this.balance -= amount;
+            targetAccount.deposit(amount);
+            transactionHistory.add(new Transfer(accountNumber, targetAccount.getAccountNumber(), amount));
+        } else {
+            throw new IllegalArgumentException("잔액이 부족합니다.");
+        }
     }
 
     public double balanceInquiry() {
         return this.balance;
     }
-
-
     public String getAccountNumber() {
         return accountNumber;
     }
