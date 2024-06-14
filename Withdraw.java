3f10a52fa3c53981dbc7b2f35c6178afb2d061cb
csjws <163653896+csjws@users.noreply.github.com>
2024-06-14 20:46:12 +0900

    Update Withdraw.java

--- Withdraw.java
+++ Withdraw.java
@@ -1,15 +1,15 @@
 
+
 public class Withdraw {
     private String withdrawID;
     private double amount;
 
     public Withdraw(String accountNumber, double amount) {
-
+        this.amount = amount;
     }
 
     public void execute(Account account, double amount) {
-
-
+        account.withdraw(amount);
     }
 
     public String getWithdrawID() {
